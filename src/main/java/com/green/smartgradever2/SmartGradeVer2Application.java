1cd866e386b28e8694ceedf513d86a6e533161de
minzion0 <aert12435@gmail.com>
2023-08-21 15:41:36 +0900

    Ver.2 시작

---
+++ src/main/java/com/green/smartgradever2/SmartGradeVer2Application.java
@@ -0,0 +1,13 @@
+package com.green.smartgradever2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGradeVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGradeVer2Application.class, args);
+    }
+
+}
