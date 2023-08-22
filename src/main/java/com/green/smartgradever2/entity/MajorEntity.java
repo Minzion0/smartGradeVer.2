6126cb328a448553a66d3c2f44cb30af12403d38
minzion0 <aert12435@gmail.com>
2023-08-22 18:54:52 +0900

    entity 설정

--- src/main/java/com/green/smartgradever2/entity/MajorEntity.java
+++ src/main/java/com/green/smartgradever2/entity/MajorEntity.java
@@ -2,6 +2,7 @@ package com.green.smartgradever2.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -10,7 +11,7 @@ import lombok.ToString;
 @Data
 @NoArgsConstructor
 @ToString(callSuper = true)
-public class MajorEntity  extends BoardEntity{
+public class MajorEntity  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED")
