d60f4e770619b74132ca50852b1c96c287321141
minzion0 <aert12435@gmail.com>
2023-08-22 18:21:10 +0900

    entity 설정

--- src/main/java/com/green/smartgradever2/entity/StudentEntity.java
+++ src/main/java/com/green/smartgradever2/entity/StudentEntity.java
@@ -1,5 +1,6 @@
 package com.green.smartgradever2.entity;
 
+import com.green.smartgradever2.entity.model.GenderEnum;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.DynamicInsert;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "student")
@@ -26,8 +29,43 @@ public class StudentEntity extends BoardEntity {
     @Size(max = 10)
     private int studentNum;
 
+    @Column(name = "student_password",nullable = false,length = 100)
+    private String studentPassword;
+
+//
+//    @Column
+//    private
+
+    @Column(name = "grade",nullable = false,length = 10,columnDefinition = "1 DEFAULT")
+    private Integer grade;
+
+    @Column(nullable = false,length = 10)
+    private String nm;
+
+    @Column(nullable = false)
+    private GenderEnum gender;
+
+
+    @Column(length = 100)
+    private String pic;
+
+    @Column(nullable = false)
+    private LocalDate birthdate;
+
     @Column
-    private String student
+    private String phone;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(length = 100)
+    private String address;
+
+    @Column(name = "finished_yn",columnDefinition = "1 DEFAULT")
+    private Integer finishedYn;
+
+
+
 
 
 
