f1034b0e80d4063eb1b85e6683f767f89c09dce0
minzion0 <aert12435@gmail.com>
2023-08-23 15:06:33 +0900

    LectureStudentEntity 테스트

---
+++ src/main/java/com/green/smartgradever2/entity/LectureStudentEntity.java
@@ -0,0 +1,66 @@
+package com.green.smartgradever2.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+@Table(name = "lecture_student")
+@EqualsAndHashCode(callSuper = true)
+@DynamicInsert
+public class LectureStudentEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ilecture_student",updatable = false,nullable = false,columnDefinition = "BIGINT UNSIGNED")
+    private Long ilectureStudent;
+
+    @ManyToOne
+    @JoinColumn(name = "studentNum")
+    @ToString.Exclude
+    private StudentEntity studentEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "ilecture")
+    @ToString.Exclude
+    private LectureAppllyEntity lectureAppllyEntity;
+
+    @Column(name = "finished_yn",nullable = false)
+    @ColumnDefault("0")
+    private int finishedYn;
+
+    @Column
+    @ColumnDefault("0")
+    private int attendance;
+
+    @Column(name = "midterm_examination")
+    @ColumnDefault("0")
+    private int midtermExamination;
+
+    @Column(name = "final_Examination")
+    @ColumnDefault("0")
+    private int finalExamination;
+
+    @Column(name = "total_score")
+    @ColumnDefault("0")
+    private int totalScore;
+
+    @Column(name = "finished_at")
+    private LocalDate finishedAt;
+
+    @Column(name = "correction_at")
+    private LocalDate correctionAt;
+
+
+
+
+}
