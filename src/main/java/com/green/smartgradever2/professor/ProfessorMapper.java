de26de260ed4c93076409b417b12f6ed6653426e
배성현 <zkti12345@naver.com>
2023-08-23 18:18:07 +0900

    교수 학생 리파지토리

--- src/main/java/com/green/smartgradever2/professor/ProfessorMapper.java
+++ src/main/java/com/green/smartgradever2/professor/ProfessorMapper.java
@@ -1,7 +1,9 @@
 package com.green.smartgradever2.professor;
 
+import com.green.smartgradever2.entity.ProfessorEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ProfessorMapper {
+    int upProfessor(ProfessorEntity entity);
 }
