0778a95da38c9bbed04bdcf5b49ad5004116a4db
진혁재 <jhj02201@gmail.com>
2023-08-24 16:26:36 +0900

    adminLectureRoom Service 및 TDD작성완료

--- src/main/java/com/green/smartgradever2/admin/lectureroom/model/AdminLectureRoomDto.java
+++ src/main/java/com/green/smartgradever2/admin/lectureroom/model/AdminLectureRoomDto.java
@@ -1,12 +1,17 @@
 package com.green.smartgradever2.admin.lectureroom.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminLectureRoomDto {
     private Long ilectureRoom;
     private String buildingName;
@@ -16,4 +21,5 @@ public class AdminLectureRoomDto {
     private int staIdx;
     private List<AdminLectureRoomListVo> lectureRoomList;
     private List<AdminLectureRoomVo> lectureRoom;
+    private AdminLectureRoomFindRes findRes;
 }
