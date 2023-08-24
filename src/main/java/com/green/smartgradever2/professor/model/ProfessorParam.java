17978f618f742c70bb4136e2edabced734c8f0ee
배성현 <zkti12345@naver.com>
2023-08-24 16:59:44 +0900

    교수 프로필 수정 사진파일 업로드및 삭제

---
+++ src/main/java/com/green/smartgradever2/professor/model/ProfessorParam.java
@@ -0,0 +1,11 @@
+package com.green.smartgradever2.professor.model;
+
+import lombok.Data;
+
+@Data
+public class ProfessorParam {
+    private String phone;
+    private String email;
+    private String address;
+    private Long iprofessor;
+}
---
+++ src/main/java/com/green/smartgradever2/professor/model/ProfessorUpdDto.java
@@ -0,0 +1,11 @@
+package com.green.smartgradever2.professor.model;
+
+import lombok.Data;
+
+@Data
+public class ProfessorUpdDto {
+    private String phone;
+    private String email;
+    private String address;
+    private Long iprofessor;
+}
