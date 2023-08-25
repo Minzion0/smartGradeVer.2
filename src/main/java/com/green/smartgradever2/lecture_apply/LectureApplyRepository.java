53534cf4c5a96736a13cd60b9b2f92bad4725fb2
배성현 <zkti12345@naver.com>
2023-08-25 14:14:06 +0900

    레파지토리 수정

--- src/main/java/com/green/smartgradever2/lecture_apply/LectureApplyRepository.java
+++ src/main/java/com/green/smartgradever2/lecture_apply/LectureApplyRepository.java
@@ -2,10 +2,13 @@ package com.green.smartgradever2.lecture_apply;
 
 import com.green.smartgradever2.entity.LectureApplyEntity;
 import com.green.smartgradever2.entity.ProfessorEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface LectureApplyRepository extends JpaRepository<LectureApplyEntity,Long> {
     List<LectureApplyEntity> findByProfessorEntity(ProfessorEntity professor);
+    Page<LectureApplyEntity> findAllByProfessorEntityIprofessor(Long iprofessor, Pageable pageable);
 }
