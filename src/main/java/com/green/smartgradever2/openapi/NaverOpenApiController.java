c2973ef47aa3f98a5aae84ebcdf11080e5554d76
minzion0 <aert12435@gmail.com>
2023-09-01 19:21:35 +0900

    open api 준비

---
+++ src/main/java/com/green/smartgradever2/openapi/NaverOpenApiController.java
@@ -0,0 +1,11 @@
+package com.green.smartgradever2.openapi;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping()
+public class NaverOpenApiController {
+}
