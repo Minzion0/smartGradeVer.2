78ca12c901280b5a3860ce0bc5b3c44d66b5c282
배성현 <zkti12345@naver.com>
2023-09-07 18:52:12 +0900

    페이지 처리

--- src/main/java/com/green/smartgradever2/professor/professorgrade/model/ProfessorStuLectureRes.java
+++ src/main/java/com/green/smartgradever2/professor/professorgrade/model/ProfessorStuLectureRes.java
@@ -1,16 +1,15 @@
 package com.green.smartgradever2.professor.professorgrade.model;
 
-import com.green.smartgradever2.student.model.StudentLectureDto;
 import com.green.smartgradever2.utils.PagingUtils;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
 
-@Data
+@Getter
 @Builder
-public class ProfessorGradeStudentDto {
+public class ProfessorStuLectureRes {
     private Long iprofessor;
     private PagingUtils page;
-    private List<ProStudentLectureDto> lectureList;
+    private List<ProfessorStudentLectureDto> lectureList;
 }
