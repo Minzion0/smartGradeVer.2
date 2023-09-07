a9b0b09e524f32911a756e0cbfb7f89512e6eb43
배성현 <zkti12345@naver.com>
2023-09-07 14:44:09 +0900

    교수성적리스트 정렬 수정

--- src/main/java/com/green/smartgradever2/lecture_apply/model/LectureSelAllRes.java
+++ src/main/java/com/green/smartgradever2/lecture_apply/model/LectureSelAllRes.java
@@ -1,5 +1,6 @@
 package com.green.smartgradever2.lecture_apply.model;
 
+import com.green.smartgradever2.utils.PagingUtils;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,5 +9,6 @@ import java.util.List;
 @Getter
 @Builder
 public class LectureSelAllRes {
+    private PagingUtils page;
     private List<LectureApplySelDto> lectureList;
 }
