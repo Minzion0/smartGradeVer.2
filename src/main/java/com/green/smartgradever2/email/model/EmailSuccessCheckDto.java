51ebb932dce47dabfa54701f4dc197db5a4ed5f4
김재경 <worud4227@naver.com>
2023-09-11 11:38:15 +0900

    email - jk

--- src/main/java/com/green/smartgradever2/email/model/EmailSuccessCheckDto.java
+++ src/main/java/com/green/smartgradever2/email/model/EmailSuccessCheckDto.java
@@ -3,7 +3,7 @@ package com.green.smartgradever2.email.model;
 import lombok.Data;
 
 @Data
-public class emailSuccessCheckDto {
+public class EmailSuccessCheckDto {
     private Long iuser;
     private String role;
 }
