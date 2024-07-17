1dc6512b1181742d98d9ef2c7d70e133ba9d1f82
minzion <aert12435@gmail.com>
2024-07-17 23:47:07 +0900

    수정

--- src/main/java/com/green/smartgradever2/admin/major/AdminMajorQdsl.java
+++ src/main/java/com/green/smartgradever2/admin/major/AdminMajorQdsl.java
@@ -1,32 +1,18 @@
 package com.green.smartgradever2.admin.major;
 
-import com.green.smartgradever2.admin.grade_mngmn.model.GradeMngmnVo;
 import com.green.smartgradever2.admin.major.model.AdminMajorDto;
 import com.green.smartgradever2.admin.major.model.AdminMajorVo;
 import com.green.smartgradever2.config.entity.QMajorEntity;
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.*;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.PathBuilder;
-import com.querydsl.core.types.dsl.SimplePath;
-import com.querydsl.core.util.StringUtils;
-import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.javassist.runtime.Desc;
-
-import org.apache.poi.ss.formula.functions.T;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static com.querydsl.core.types.dsl.Expressions.nullExpression;
-import static com.querydsl.core.types.dsl.Expressions.simplePath;
 
 @Slf4j
 @Component
@@ -35,18 +21,19 @@ public class AdminMajorQdsl {
     private final JPAQueryFactory jpaQueryFactory;
     QMajorEntity m = QMajorEntity.majorEntity;
 
+    /** 전공 목록 조회 **/
     public List<AdminMajorVo> majorVos(AdminMajorDto dto, Pageable pageable) {
         JPAQuery<AdminMajorVo> query = jpaQueryFactory.select(Projections.bean(AdminMajorVo.class, m.imajor
-                        , m.majorName, m.graduationScore, m.delYn, m.remarks))
-                .from(m)
-                .where(eqMajorName(dto.getMajorName())
-                ,eqDelYn(dto.getDelYn()))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .orderBy(m.imajor.desc());
+                , m.majorName, m.graduationScore, m.delYn, m.remarks))
+            .from(m)
+            .where(eqMajorName(dto.getMajorName()), eqDelYn(dto.getDelYn()))
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy(m.imajor.desc());
         return query.fetch();
     }
 
+    /** 전공 이름 조건 **/
     private BooleanExpression eqMajorName(String name) {
         if (StringUtils.isNullOrEmpty(name)) {
             return null;
@@ -54,6 +41,7 @@ public class AdminMajorQdsl {
         return m.majorName.contains(name);
     }
 
+    /** 삭제 여부 조건 **/
     private BooleanExpression eqDelYn(Integer delYn) {
         if (delYn == null) {
             return null;
